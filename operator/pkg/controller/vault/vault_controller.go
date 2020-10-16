13897c1c8e5502436b3a19e644d77dc4b5df26e3
Nandor Kracser <bonifaido@gmail.com>
2020-10-16 15:52:25 +0200

    fix crd generation and types
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- operator/pkg/controller/vault/vault_controller.go
+++ operator/pkg/controller/vault/vault_controller.go
@@ -20,7 +20,6 @@ import (
 	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -2001,9 +2000,7 @@ func withNamespaceEnv(v *vaultv1alpha1.Vault, envs []corev1.EnvVar) []corev1.Env
 }
 
 func withContainerSecurityContext(v *vaultv1alpha1.Vault) *corev1.SecurityContext {
-	var config map[string]interface{}
-	_ = json.Unmarshal(v.Spec.Config.Raw, &config)
-
+	config := v.Spec.GetVaultConfig()
 	if cast.ToBool(config["disable_mlock"]) {
 		return &corev1.SecurityContext{}
 	}
